c84ba69d006f624aacc896fdbd07e9cc3bdeb9c9
Paluffel <40839103+Paluffel@users.noreply.github.com>
2019-04-01 19:23:53 +0200

    Update app.ts

--- faes attachment stuff/src/app.ts
+++ faes attachment stuff/src/app.ts
@@ -83,7 +83,7 @@ export default class HelloWorld {
                            }
                      }
                  }).value;
-                 model.attach(userId, "hips");
+                 model.attach(userId, "right-lower-leg");
                 })
             }
         })
